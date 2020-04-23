0905fd3325cf04d77f68e468cc4f0c037ac094f8
unknown <drizzzt@gmail.com>
2020-04-22 23:01:16 -0300

    V1.0 first attempt
    first commit of my personal website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($){
+    var tped = new Typed('span.txt-rotate',{
+        strings: ["Soy un desarrollador web.", "Soy programador.", "Soy  ingeniero en informática."],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true
+    });
+})(jQuery)
