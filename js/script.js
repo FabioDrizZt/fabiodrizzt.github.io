d49b0d758e403df8b599623af6ec2c48f94935cb
Fabio DrizZt <drizzzt@gmail.com>
2020-10-08 14:08:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 (function($){
     var tped = new Typed('span.txt-rotate',{
-        strings:   ["Soy desarrollador web.", 
-                    "Soy docente en educación Superior.",
-                    "Soy programador.",  
-                    "Soy ingeniero informático."],
+        strings:   ["Desarrollador Web Full Stack @ soyHenry ~ P.E.R.N. Stack.", 
+                    "Docente Universitario: Programación, BBDD, Simulación.",
+                    "Ingeniero en Informática graduado en la FI-UNJu.",  
+                    "Gamer, Nadador, Amante de las Series y Animes."],
         typeSpeed: 100,
         backSpeed: 100,
         fadeOut: false,
